9d2f1769c30fa5e1871202decb4b32e1b437fee9
Lina Al Taleb <113062307+AlTalebLina@users.noreply.github.com>
2025-03-22 08:08:06 -0400

    mods

--- myscript.py
+++ myscript.py
@@ -1,4 +1,11 @@
 import os
 
-os.system("git bisect start $badhash $goodhash")
+# Définitions en dur des commits "good" et "bad"
+goodhash = "e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c"
+badhash = "c1a4be04b972b6c17db242fc37752ad517c29402"
+
+# Démarrer le bisect avec les commits indiqués
+os.system(f"git bisect start {badhash} {goodhash}")
+
+# Lancer automatiquement la commande de test sur chaque commit
 os.system("git bisect run python manage.py test")
